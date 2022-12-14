914e6de6bcc9dc522f0217bcd4eecb7e53d19363
SaraswathiRana <saraswathi.rana62@gmail.com>
2022-12-14 06:08:29 +0530

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,16 @@
+import json
+
+def lambda_handler(event, context):
+    content = """
+    
+    
+    
+    
+    
+    # TODO implement
+    response = {
+        'statusCode': 200,
+        'body': content,
+        "headers": {"Content-Type": "text/html",},
+    }
+    return response
